aa58bb74bf6218cb519378b2245e2d83344bf55c
Christofer Nyström <christofer.nystrom@iths.se>
2023-05-06 16:16:44 +0200

    Adds sleep for 500ms

--- src/main/java/com/example/urlshortenerservice/service/Publisher.java
+++ src/main/java/com/example/urlshortenerservice/service/Publisher.java
@@ -1,12 +1,18 @@
 package com.example.urlshortenerservice.service;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class Publisher{
 
     RabbitTemplate rabbitTemplate;
+    String MESSAGE_QUEUE = "messages";
 
     public Publisher(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
@@ -15,6 +21,29 @@ public class Publisher{
     public void publishMessage(String message) {
         //rabbitTemplate.convertAndSend("urlShortenerExchange", "urlShortenerRoutingKey", message);
 
-        rabbitTemplate.convertAndSend("messages", message);
+        rabbitTemplate.convertAndSend(MESSAGE_QUEUE, message);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(MESSAGE_QUEUE);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(MESSAGE_QUEUE);
+        container.setMessageListener(message -> {
+            try {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Message " + new String(message.getBody()));
+        });
+
+        return container;
     }
 }
