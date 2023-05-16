e0491615bb7dca4082959bed97442098cff174cc
RobertMili <robertmilicevic3869@gmail.com>
2023-05-16 13:33:00 +0200

    rabbitmq working

--- src/main/java/com/example/urlshortenerservice/UrlShortenerServiceApplication.java
+++ src/main/java/com/example/urlshortenerservice/UrlShortenerServiceApplication.java
@@ -1,13 +1,24 @@
 package com.example.urlshortenerservice;
 
+import com.rabbitmq.client.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class UrlShortenerServiceApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerServiceApplication.class, args);
+
+        RabbitConnection rabbitConnection = new RabbitConnection();
+        rabbitConnection.startConsuming();
+
     }
 
 }
+
