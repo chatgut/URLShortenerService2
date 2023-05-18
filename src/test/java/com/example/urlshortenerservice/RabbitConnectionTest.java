0854595a13df5e65ef03fc08ce602c710ea62530
RobertMili <robertmilicevic3869@gmail.com>
2023-05-18 13:45:43 +0200

    Now is working 1. Get message from post service/ rabbitMQ 2. Convert to short Link 3. Send to rabbitMQ

--- src/test/java/com/example/urlshortenerservice/RabbitConnectionTest.java
+++ src/test/java/com/example/urlshortenerservice/RabbitConnectionTest.java
@@ -4,18 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RabbitConnectionTest {
 
-    public static void main(String[] args) {
-
-
-        RabbitConnection rabbitConnection = new RabbitConnection();
-
-        rabbitConnection.publishMessage();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//
+//
+//        RabbitConnection rabbitConnection = new RabbitConnection();
+//
+//        rabbitConnection.publishMessage();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
