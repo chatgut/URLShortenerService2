4d3c752e0aa75d021fa20be007e57f51321ae53e
RobertMili <robertmilicevic3869@gmail.com>
2023-05-28 10:47:35 +0200

    testing graalVM

--- src/main/java/com/example/urlshortenerservice/UrlShortenerServiceApplication.java
+++ src/main/java/com/example/urlshortenerservice/UrlShortenerServiceApplication.java
@@ -2,26 +2,29 @@ package com.example.urlshortenerservice;
 
 
 
-import com.example.urlshortenerservice.rabbitMQ.RabbitConnection;
-import org.springframework.beans.factory.annotation.Autowired;
+//import com.example.urlshortenerservice.rabbitMQ.RabbitConnection;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
+//@SpringBootApplication
+//public class UrlShortenerServiceApplication implements CommandLineRunner{
 
-@SpringBootApplication
-public class UrlShortenerServiceApplication implements CommandLineRunner{
+    @SpringBootApplication
+    public class UrlShortenerServiceApplication{
 
-    @Autowired
-    private RabbitConnection rabbitConnection;
+//    @Autowired
+//    private RabbitConnection rabbitConnection;
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerServiceApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        rabbitConnection.startConsuming();
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        rabbitConnection.startConsuming();
+//    }
 }
 
--- src/main/java/com/example/urlshortenerservice/rabbitMQ/RabbitConnection.java
+++ src/main/java/com/example/urlshortenerservice/rabbitMQ/RabbitConnection.java
@@ -1,126 +1,126 @@
-package com.example.urlshortenerservice.rabbitMQ;
-
-import com.example.urlshortenerservice.dto.UrlDto;
-import com.example.urlshortenerservice.dto.UrlResponseDto;
-import com.example.urlshortenerservice.entity.Url;
-
-import com.example.urlshortenerservice.service.UrlService;
-import com.rabbitmq.client.*;
-
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.concurrent.TimeoutException;
-
-
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-
-
-
-@Configuration
-public class RabbitConnection {
-    private Connection connection;
-    private Channel channel;
-    private static final String QUEUE_MESSAGE_GET = "messages";
-    private static final String QUEUE_NAME_SENDING_MESSAGE = "shortUrl";
-
-    private String messageConvert;
-
-    UrlDto urlDto = new UrlDto();
-
-    @Autowired
-    public UrlService urlService;
-
-
-    public RabbitConnection() {
-        try {
-
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost(System.getenv().getOrDefault("ROCKET_RABBIT_HOST", "localhost"));
-            factory.setPort(5672);
-
-            connection = factory.newConnection();
-            channel = connection.createChannel();
-
-
-            channel.queueDeclare(QUEUE_MESSAGE_GET, false, false, false, null);
-        } catch (IOException | TimeoutException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public void startConsuming() {
-        try {
-            channel.basicConsume(QUEUE_MESSAGE_GET, true, new DefaultConsumer(channel) {
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope,
-                                           AMQP.BasicProperties properties, byte[] body) {
-                    String message = new String(body, StandardCharsets.UTF_8);
-                    JSONObject jsonPayload = new JSONObject(message);
-                    String extractedMessage = jsonPayload.getString("message");
-
-
-                    if (checkingForHttpAndHttps(extractedMessage)) {
-
-                        messageConvert = extractedMessage;
-                        UrlResponseDto urlResponseDto = addingInDatabaseMessage();
-
-                        System.out.println("Received message: " + messageConvert);
-
-                        publishMessage(urlResponseDto.getShortLink());
-                    }
-
-                }
-
-                private UrlResponseDto addingInDatabaseMessage() {
-                    urlDto.setUrl(messageConvert);
-
-                    Url urlToRet = urlService.generateSHortLink(urlDto);
-
-                    UrlResponseDto urlResponseDto = new UrlResponseDto();
-                    urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
-                    urlResponseDto.setExpirationDate(LocalDateTime.now());
-                    urlResponseDto.setShortLink(urlToRet.getShortLink());
-                    return urlResponseDto;
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private boolean checkingForHttpAndHttps(String extractedMessage) {
-        String httpUrlPattern = "(?i)\\bhttps?://\\S+\\b";
-
-        return extractedMessage.matches(httpUrlPattern);
-    }
-
-    public void publishMessage(String sendingMessage) {
-        try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost(System.getenv().getOrDefault("ROCKET_RABBIT_HOST", "localhost"));
-            factory.setPort(5672);
-
-            try (Connection connection = factory.newConnection();
-                 Channel channel = connection.createChannel()) {
-                channel.queueDeclare("shortURL", false, false, false, null);
-
-                JSONObject jsonAd = new JSONObject();
-                jsonAd.put("message", sendingMessage);
-
-                String message = jsonAd.toString();
-
-
-                channel.basicPublish("", QUEUE_NAME_SENDING_MESSAGE, null, message.getBytes(StandardCharsets.UTF_8));
-                System.out.println("Sent message to RabbitMQ: " + message);
-            }
-        } catch (TimeoutException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
+//package com.example.urlshortenerservice.rabbitMQ;
+//
+//import com.example.urlshortenerservice.dto.UrlDto;
+//import com.example.urlshortenerservice.dto.UrlResponseDto;
+//import com.example.urlshortenerservice.entity.Url;
+//
+//import com.example.urlshortenerservice.service.UrlService;
+//import com.rabbitmq.client.*;
+//
+//
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.time.LocalDateTime;
+//import java.util.concurrent.TimeoutException;
+//
+//
+//import org.json.JSONObject;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//
+//@Configuration
+//public class RabbitConnection {
+//    private Connection connection;
+//    private Channel channel;
+//    private static final String QUEUE_MESSAGE_GET = "messages";
+//    private static final String QUEUE_NAME_SENDING_MESSAGE = "shortUrl";
+//
+//    private String messageConvert;
+//
+//    UrlDto urlDto = new UrlDto();
+//
+//    @Autowired
+//    public UrlService urlService;
+//
+//
+//    public RabbitConnection() {
+//        try {
+//
+//            ConnectionFactory factory = new ConnectionFactory();
+//            factory.setHost(System.getenv().getOrDefault("ROCKET_RABBIT_HOST", "localhost"));
+//            factory.setPort(5672);
+//
+//            connection = factory.newConnection();
+//            channel = connection.createChannel();
+//
+//
+//            channel.queueDeclare(QUEUE_MESSAGE_GET, false, false, false, null);
+//        } catch (IOException | TimeoutException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//    public void startConsuming() {
+//        try {
+//            channel.basicConsume(QUEUE_MESSAGE_GET, true, new DefaultConsumer(channel) {
+//                @Override
+//                public void handleDelivery(String consumerTag, Envelope envelope,
+//                                           AMQP.BasicProperties properties, byte[] body) {
+//                    String message = new String(body, StandardCharsets.UTF_8);
+//                    JSONObject jsonPayload = new JSONObject(message);
+//                    String extractedMessage = jsonPayload.getString("message");
+//
+//
+//                    if (checkingForHttpAndHttps(extractedMessage)) {
+//
+//                        messageConvert = extractedMessage;
+//                        UrlResponseDto urlResponseDto = addingInDatabaseMessage();
+//
+//                        System.out.println("Received message: " + messageConvert);
+//
+//                        publishMessage(urlResponseDto.getShortLink());
+//                    }
+//
+//                }
+//
+//                private UrlResponseDto addingInDatabaseMessage() {
+//                    urlDto.setUrl(messageConvert);
+//
+//                    Url urlToRet = urlService.generateSHortLink(urlDto);
+//
+//                    UrlResponseDto urlResponseDto = new UrlResponseDto();
+//                    urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
+//                    urlResponseDto.setExpirationDate(LocalDateTime.now());
+//                    urlResponseDto.setShortLink(urlToRet.getShortLink());
+//                    return urlResponseDto;
+//                }
+//            });
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private boolean checkingForHttpAndHttps(String extractedMessage) {
+//        String httpUrlPattern = "(?i)\\bhttps?://\\S+\\b";
+//
+//        return extractedMessage.matches(httpUrlPattern);
+//    }
+//
+//    public void publishMessage(String sendingMessage) {
+//        try {
+//            ConnectionFactory factory = new ConnectionFactory();
+//            factory.setHost(System.getenv().getOrDefault("ROCKET_RABBIT_HOST", "localhost"));
+//            factory.setPort(5672);
+//
+//            try (Connection connection = factory.newConnection();
+//                 Channel channel = connection.createChannel()) {
+//                channel.queueDeclare("shortURL", false, false, false, null);
+//
+//                JSONObject jsonAd = new JSONObject();
+//                jsonAd.put("message", sendingMessage);
+//
+//                String message = jsonAd.toString();
+//
+//
+//                channel.basicPublish("", QUEUE_NAME_SENDING_MESSAGE, null, message.getBytes(StandardCharsets.UTF_8));
+//                System.out.println("Sent message to RabbitMQ: " + message);
+//            }
+//        } catch (TimeoutException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+//
